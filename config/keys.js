1471baec21c2fbdaf741b62bbcd37fac74826e17
nancyzhe <60163932+nancyzhe@users.noreply.github.com>
2021-09-17 14:53:59 -0400

    Added signin and signup functionality

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+import "firebase/auth"
+import "firebase/firestore"
+
+export default apiKeys = {
+    firebaseConfig: {
+    apiKey: "AIzaSyDyXuRrPDxYFQEUT4qJDjhM6lzdx3sRnU8",
+    authDomain: "barcode-miniproject.firebaseapp.com",
+    projectId: "barcode-miniproject",
+    storageBucket: "barcode-miniproject.appspot.com",
+    messagingSenderId: "59732661679",
+    appId: "1:59732661679:web:7ab3faaf49809a9ef6cb5a",
+    measurementId: "G-CYXHYG9Q0C"
+    }
+};
---
+++ stack/FinalStackScreen.js
@@ -0,0 +1,67 @@
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import WelcomeScreen from '../screens/WelcomeScreen';
+import SignUp from '../screens/SignUp';
+import SignIn from '../screens/SignIn';
+import LoadingScreen from '../screens/LoadingScreen';
+import Dashboard from '../screens/Dashboard';
+//import ProfileScreen from './screens/ProfileScreen';
+import MainScreen from '../screens/MainScreen';
+import ScanScreen from '../screens/ScanScreen';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function FinalStackScreen() {
+
+    function HomeTabs() {
+        return (
+            <Tab.Navigator screenOptions={{ headerShown: false }}>
+                <Tab.Screen 
+                name='Me' 
+                component={Dashboard} 
+                options={{
+                tabBarIcon: (size) => (
+                    <Ionicons name = "ios-person" size = {28}/>
+                ),
+                }}
+            />
+                <Tab.Screen 
+                name='Main' 
+                component={MainScreen} 
+                options={{
+                tabBarIcon: (size) => (
+                    <Ionicons name = "home" size = {30}/>
+                ),
+                }}
+            />
+                <Tab.Screen 
+                name='Scan' 
+                component={ScanScreen}  
+                options={{
+                tabBarIcon: (size) => (
+                <Ionicons name = "barcode-sharp" size = {30}/>
+                ),
+            }}
+            />
+            </Tab.Navigator>
+        ); 
+    }
+        return (
+            <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name={'Loading'} component={LoadingScreen} options={{ headerShown: false }}/>
+                <Stack.Screen name='Home' component={WelcomeScreen} options={{ headerShown: false }}/>
+                <Stack.Screen name='Sign Up' component={SignUp} options={{ headerShown: false }}/>
+                <Stack.Screen name='Sign In' component={SignIn} options={{ headerShown: false }}/>
+                <Stack.Screen name={'Dashboard'} component={HomeTabs}  />
+            </Stack.Navigator>
+            </NavigationContainer>
+        );
+}
