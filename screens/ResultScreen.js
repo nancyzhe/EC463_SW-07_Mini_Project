dca8788e5eed9551bcc29958140bd302437372fb
Haoxuan Li <harryli@bu.edu>
2021-09-19 18:13:43 -0400

    Result saved into db
    Result saved into db

--- screens/ResultScreen.js
+++ screens/ResultScreen.js
@@ -4,6 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import {temp} from '../screens/ScanScreen';
 
+import * as firebase from 'firebase';//import firebase
+import 'firebase/firestore';//import firebase
 
 export default function ResultScreen() {
     const [data, setData] = useState([]);
@@ -14,6 +16,23 @@ export default function ResultScreen() {
             .then((json) => setData(json.foods[0]))
             .finally(() => setLoading(false));
     })
+
+    const db = firebase.firestore();
+    const currentUser = firebase.auth().currentUser;
+    if (setLoading === false) { // if Data is loaded
+        let food_name = data.description; //save food name into variable "food_name"
+        var length = Object.keys(data.foodNutrients).length;// getting the json array length for nutrient array
+        for (let i = 0; i < length; i++) { // looping through all nutrients
+            db.collection('users')
+                .doc(currentUser.uid)
+                .collection(food_name)//creating a collection under the current user uid and name it the food's name
+                .doc(data.foodNutrients[i].nutrientName) // for each nutrient, create a doc with the name of the nutrient
+                .set({
+                    value: data.foodNutrients[i].value, // set value
+                    unit: data.foodNutrients[i].unitName, //set unit
+                });
+        }
+    }
 
     return (
         <SafeAreaView>
